e9c03e13086c63f965d77ddcde965d3b40039b7e
yassirelkhaili <elkhailiyassir@gmail.com>
2023-12-14 00:51:23 +0100

    migration wizard almost done

--- src/SimpleORM/EntityManager.php
+++ src/SimpleORM/EntityManager.php
@@ -2,6 +2,24 @@
 
 namespace EntityManager;
 
+require "../Database/connections/conn.php";
+require "./QueryGenerator.php";
+
+use PDO, queries\QueryGenerator;
 class EntityManager {
-    
+    private $db;
+
+    public function __construct(PDO $db) {
+        $this->db = $db;
+    }
+    public function createTable(array $data): void {
+        $query = QueryGenerator::generateTableQuery($data);
+        $stmt = $this->db->prepare($query);
+        if ($stmt === false) {
+            throw new \Exception("Error preparing statement");
+        }
+        if (!$stmt->execute() === false) {
+            throw new \Exception("Error creating the table");
+        }
+    }
 }
--- src/Utils/EntityMapper.php
+++ src/Utils/EntityMapper.php
@@ -9,20 +9,34 @@ class Entity {
 }
 
 class EntityMapper {
-    private static Entity $entity;
-    public static function setEntity(Entity $entity): void {
-        self::$entity = $entity;
+    private Entity $entity;
+    public function __construct(Entity $entity) {
+        $this->entity = $entity;
     }
-
     public function map(): array {
-        $entity_name = get_class(self::$entity);
+        $entity_name = get_class($this->entity);
         $reflection_class = new ReflectionClass($entity_name);
         $entity_properties = $reflection_class->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PRIVATE | ReflectionProperty::IS_PROTECTED);
-        $property_names = array_map(fn ($property) => $property->getName(), $entity_properties);
+        $property_mapping = [];
+        foreach ($entity_properties as $property) {
+            $property_name = $property->getName();
+            $property_type = $this->getPropertyType($property);
+            $property_mapping[$property_name] = [
+                'type' => $property_type,
+            ];
+        }
         $entity_mapping = [
             "entityName" => $entity_name,
-            "entityProperties" => $property_names,
+            "entityProperties" => $property_mapping,
         ];
-        return $entity_mapping;
+       return $entity_mapping;
+    }
+    private function getPropertyType(ReflectionProperty $property): ?string {
+        $type = null;
+        if ($property->hasType()) {
+            $property_type = $property->getType();
+            $type = $property_type->isBuiltin() ? $property_type->getName() : null;
+        }
+        return $type;
     }
 }
