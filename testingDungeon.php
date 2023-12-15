9216e0383879afc30ad97214139f347fc599eccb
yassirelkhaili <elkhailiyassir@gmail.com>
2023-12-15 22:54:00 +0100

    finished batch insert

--- testingDungeon.php
+++ testingDungeon.php
@@ -5,7 +5,38 @@ require "./src/SimpleORM/EntityManager.php";
 use EntityManager\EntityManager;
 
 $entity = new EntityManager($conn, "Users");
-$entity->name = "nameqwd";
-$entity->email = "email3";
 
-$entity->save();
+// individual insert
+// $entity->email = "type";
+// $entity->name = "name421";
+// $entity->lastname = "last_name2";
+
+// $entity->save();
+
+//batch insert
+
+//example use
+// $entity->saveMany([
+//     ["columnName"=> "exampleValue1","columnName2"=> "exampleValue2"],
+// ]);
+
+$entity->saveMany([
+    ["name"=> "nameqwd","email"=> "email@gmail.com", "lastname" => "lastname"],
+    ["name"=> "nameqwd2","email"=> "email2@gmail.com", "lastname" => "lastname"],
+    ["name"=> "nameqwd3","email"=> "email3@gmail.com", "lastname" => "lastname"],
+]);
+
+$entity->flush();
+
+// $array = [
+//         ["name"=> "nameqwd","email"=> "email@gmail.com", "lastname" => "lastname"],
+//         ["name"=> "nameqwd2","email"=> "email2@gmail.com", "lastname" => "lastname"],
+//         ["name"=> "nameqwd3","email"=> "email3@gmail.com", "lastname" => "lastname"],
+// ];
+// $index = 0;
+
+// foreach ($array as $item) {
+//     $keys = array_keys($item);
+//     print_r($item[$index]);
+//     $index++;
+// }
